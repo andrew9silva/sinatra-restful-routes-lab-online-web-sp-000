26b8964a40fc204b00972f95ae51520b149802cb
Andrew Silva <andrewsilva9@gmail.com>
2019-11-23 00:43:46 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,7 +12,7 @@ class ApplicationController < Sinatra::Base
   post '/recipes' do
     
     @recipe = Recipe.create(params)
-    redirect '/recipes/#{@recipe.id}' 
+    redirect "/recipes/#{@recipe.id}"
   end
   
   get '/recipes/new' do
